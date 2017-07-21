dcefe0a084dc31339840a88b9a292868a9641811
Oleg Maksimov <ganeroth@gmail.com>
2017-07-22 00:18:34 +0300

    added some code

--- crawler/src/components/Parser.java
+++ crawler/src/components/Parser.java
@@ -1,6 +1,17 @@
 package components;
 
+import java.util.Map;
+
 public class Parser {
-    //todo
-    //todo
+    private Map<String, String> wordsForParse;
+    private String pageContent;
+
+    public Parser(Map<String, String> nameTags, String pageContent){
+        this.wordsForParse = nameTags;
+        this.pageContent = pageContent;
+    }
+
+    public Map<String, Integer> getParseResult(){
+        return null;
+    }
 }
