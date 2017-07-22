1da28dd8d3e978a8012329e49300e21db8a0f317
Oleg Maksimov <ganeroth@gmail.com>
2017-07-22 07:41:21 +0300

    completed first version of Parser module

--- crawler/src/main/java/components/Parser.java
+++ crawler/src/main/java/components/Parser.java
@@ -1,53 +1,55 @@
 package components;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
     private Map<String, String[]> wordsForParse;
     private String pageContent;
-    private Map<String, Integer>parseResultMap;
+    private Map<String, Integer> parseResultMap;
+    private ExecutorService executorService = Executors.newFixedThreadPool(8);
 
 
-    public Parser(Map<String, String[]> nameTags, String pageContent){
+    public Parser(Map<String, String[]> nameTags, String pageContent) {
         this.wordsForParse = nameTags;
         this.pageContent = pageContent;
     }
 
-    public Map<String, Integer> getParseResult(){
-        parseResultMap = new ConcurrentHashMap<String, Integer>(wordsForParse.size());
+    public Map<String, Integer> getParseResult() {
+        parseResultMap = new ConcurrentHashMap<>(wordsForParse.size());
 
-        for (Map.Entry <String, String[]> mapElement : wordsForParse.entrySet()){
-            int p = getMatchCounts(mapElement.getValue());
-            parseResultMap.put(mapElement.getKey(), p);
+        for (Map.Entry<String, String[]> mapElement : wordsForParse.entrySet()) {
+            executorService.execute(() -> {
+                int p = getMatchCounts(mapElement.getValue());
+                parseResultMap.put(mapElement.getKey(), p);
+            });
         }
-
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
         return parseResultMap;
     }
 
-    private int getMatchCounts(String[] words){
+    private int getMatchCounts(String[] words) {
         int count = 0;
-        for (String word : words){
-            Pattern pattern = Pattern.compile("(\\W|^)"+word+"(\\W|$)", Pattern.UNICODE_CHARACTER_CLASS);
+        for (String word : words) {
+            Pattern pattern = Pattern.compile("(\\W|^)" + word + "(\\W|$)", Pattern.UNICODE_CHARACTER_CLASS);
             System.out.println(pattern);
             Matcher matcher = pattern.matcher(pageContent);
-            while (matcher.find()){
+            while (matcher.find()) {
                 count++;
             }
         }
         System.out.println(count);
         return count;
-    }
-
-    public static void main(String[] args) {
-        Map<String, String[]> testMap = new HashMap<String, String[]>();
-        testMap.put("петруха", new String[]{"петр", "петру", "петром", "петра"});
-//        testMap.put("васька", new String[]{"васька", "ваське", "васькой", "васьки"});
-        String testString = "васька для петра съел бабку, и сказал петр ваське: \"ну ты охламон васькас, нету у меня больше васьки\". И васька, обиженный петром, пошел спать.";
-        Parser test = new Parser(testMap, testString);
-        System.out.println(test.getParseResult());
     }
 }
