fce88df61319ad78410642fb683d5ad824f398b5
Oleg Maksimov <ganeroth@gmail.com>
2017-07-30 19:27:25 +0300

    created basic  structure of main class

--- crawler/src/main/java/Main.java
+++ crawler/src/main/java/Main.java
@@ -1,2 +1,29 @@
+import dao.DatabaseInerface;
+import dao.MySQLDao;
+
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
+
+    private static DatabaseInerface db = new MySQLDao();
+    private static ConcurrentHashMap<String, String[]> keywords;
+    private static Vector<String> sites;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void runSiteCheckThread(){
+
+    }
+
+    private void runKeywordsCheckThread(){
+
+    }
+
+    private void getCwrawlResult(){
+
+    }
+
 }
