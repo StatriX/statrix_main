b82ba666ead734f627b5b97477f27b3b3979a1c4
Oleg Maksimov <ganeroth@gmail.com>
2017-07-30 19:29:55 +0300

    added daemon threads to check methods

--- crawler/src/main/java/Main.java
+++ crawler/src/main/java/Main.java
@@ -15,11 +15,25 @@ public class Main {
     }
 
     private void runSiteCheckThread(){
+        Thread checkSitesListFromDB = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
+        checkSitesListFromDB.setDaemon(true);
+        checkSitesListFromDB.run();
     }
 
     private void runKeywordsCheckThread(){
+        Thread checkKeywordsFromDB = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
+        checkKeywordsFromDB.setDaemon(true);
+        checkKeywordsFromDB.run();
     }
 
     private void getCwrawlResult(){
