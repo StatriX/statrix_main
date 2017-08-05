0bbb13f9daabe3fe729b9715ac3b7b6298d10e68
tremoctopus <tremoctopus@rambler.ru>
2017-08-05 22:53:11 +0300

    один из новых контроллеров

---
+++ web-service/src/main/java/com/statrix/rest/controllers/GetAdminInfo.java
@@ -0,0 +1,28 @@
+package com.statrix.rest.controllers;
+
+import com.statrix.rest.models.Users;
+import com.statrix.rest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/info/")
+public class GetAdminInfo {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "admin/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getAdminInfo(@PathVariable("id") long id){
+        Users admin = userService.findById(id);
+        if( admin == null){
+            return new ResponseEntity("not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Users>(admin, HttpStatus.OK);
+    }
+}
