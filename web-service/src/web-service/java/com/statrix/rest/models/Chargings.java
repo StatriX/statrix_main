57e18b8905e9eb2e7403e333a094e77b8b057836
Viterg <viterg@bk.ru>
2017-08-06 11:43:57 +0300

    исправлены косяки структуры

--- web-service/src/web-service/java/com/statrix/rest/models/Chargings.java
+++
@@ -1,9 +0,0 @@
-package com.statrix.rest.models;
-// модель не нужна
-public class Chargings {
-    private int id;
-    private String title;
-    private short sitesCount;
-    private short personsCount;
-    private short usersCount;
-}
--- web-service/src/web-service/java/com/statrix/rest/models/SubscriptionPerson.java
+++
@@ -1,6 +0,0 @@
-package com.statrix.rest.models;
-
-public class SubscriptionPerson {
-    private int subscriptionId;
-    private int personId;
-}
